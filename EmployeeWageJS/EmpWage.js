93727f6ac3ac4de1fbe1415a41c00e0c919211df
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-08 13:10:25 +0530

    [jogan] Add : Employee Wage computation

--- EmployeeWageJS/EmpWage.js
+++ EmployeeWageJS/EmpWage.js
@@ -14,3 +14,20 @@ if (empCheck == IS_ABSENT) {
 else {
     console.log("Employee is present");
 }
+let emphrs = 5;
+
+empCheck = Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+    case IS_PART_TIME: emphrs = PART_TIME_HR;
+
+        break;
+
+    case IS_FULL_TIME: emphrs = FULL_TIME_HR
+
+        break;
+
+    case IS_ABSENT: emphrs = IS_ABSENT;
+}
+let empWage = emphrs * WAGE_PER_HR;
+console.log(empWage);
